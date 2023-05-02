f66097bc489035f42dadef8aa48b9e77ea340be2
yunahhh <132349683+yunahhh@users.noreply.github.com>
2023-05-02 18:52:51 +0800

    Delete 編輯作業二 directory

--- 編輯作業二/main.js
+++
@@ -1,6 +0,0 @@
-// 選到class='navbar-btn"的按鈕並為其綁定click事件
-document.querySelector(".navbar-btn").addEventListener("click", function () {
-    // 表達點擊後要做的動作
-    // 選到 class="navbar-list"並為其切換 active 的 class
-    document.querySelector(".navbar-list").classList.toggle("active")
-});
